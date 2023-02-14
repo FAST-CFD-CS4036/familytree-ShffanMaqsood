d32309a7b0984c4ee6663df38292c3873dcb0c18
Shffan Maqsood <mshffan0@gmail.com>
2023-02-14 19:46:08 +0500

    third commit

--- src/bll/PersonBO.java
+++ src/bll/PersonBO.java
@@ -1,11 +1,10 @@
 package bll;
 
-
 import dal.IDAL;
 import dal.PersonDAOStub;
 import to.PersonTO;
 
-public class PersonBO implements IBO{
+public class PersonBO implements IBO {
 	private IDAL dao;
 
 	/*
@@ -18,41 +17,36 @@ public class PersonBO implements IBO{
 //	public PersonBO() {
 //	}
 
-	public boolean isSibling (int cnic1, int cnic2) {
+	public boolean isSibling(int cnic1, int cnic2) {
 		PersonTO p1 = dao.getPerson(cnic1);
 		PersonTO p2 = dao.getPerson(cnic2);
-		
+
 		return isSameFather(p1, p2) && isSameMother(p1, p2);
 	}
 
-	
-	
-	public boolean isHalfSibling (int cnic1, int cnic2) {
+	public boolean isHalfSibling(int cnic1, int cnic2) {
 		PersonTO p1 = dao.getPerson(cnic1);
 		PersonTO p2 = dao.getPerson(cnic2);
-		
-		return isSameFather(p1, p2) || isSameMother(p1, p2);
+		if((isSameFather(p1, p2) && isSameMother(p1, p2))  || (!isSameFather(p1, p2) && !isSameMother(p1, p2)))
+		{
+			return false;
+		}
+		else {
+			return true;
+		}
 	}
-	
-	
-	public PersonTO getPerson(String Name)
-	{
+
+	public PersonTO getPerson(String Name) {
 		PersonTO obj = dao.getMatchingPerson(Name);
 		return obj;
 	}
-	
+
 	private boolean isSameMother(PersonTO p1, PersonTO p2) {
 		return p1.getMother() == p2.getMother();
 	}
+
 	private boolean isSameFather(PersonTO p1, PersonTO p2) {
 		return p1.getFather() == p2.getFather();
 	}
 
-	/*
-	 * public static void main(String[] args) { PersonDAOStub daoStub = new
-	 * PersonDAOStub(); PersonBO bo = new PersonBO(daoStub); boolean expected =
-	 * true; boolean actual = bo.isSibling(2, 4); if (expected == actual) {
-	 * System.out.println("Test case passed"); } else {
-	 * System.out.println("Test case failed"); } assert expected == actual; }
-	 */
 }
--- src/dal/PersonDAOStub.java
+++ src/dal/PersonDAOStub.java
@@ -12,10 +12,13 @@ public class PersonDAOStub implements IDAL {
 	public PersonDAOStub() {
 		super();
 		db = new Hashtable<Integer, PersonTO>();
-		db.put(1, new PersonTO(1, "Shah Jahan", -1, -1));
-		db.put(2, new PersonTO(2, "Mumtaz Mahal", -1, -1));
+		db.put(1, new PersonTO(1, "Shah Jahan", 11, 12));
+		db.put(2, new PersonTO(2, "Mumtaz Mahal", 13, 14));
 		db.put(3, new PersonTO(3, "Aurangzeb", 1, 2));
 		db.put(4, new PersonTO(4, "Dara Shikoh", 1, 2));
+		db.put(5, new PersonTO(5, "Dara Umar", 1, 3));
+		db.put(6, new PersonTO(6, "Lala Umar", 4, 2));
+		db.put(7, new PersonTO(7, "Lamba Umar", 5, 6));
 	}
 
 	@Override
